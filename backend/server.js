5a914fa8745de6ee276af7b125d1ed656c7f722b
aashish-1303 <aashishmehbubani2@gmail.com>
2026-03-06 14:55:53 +0530

    Aded a feature for adding notes

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,17 @@ app.get("/notes", (req, res) =>
     res.json(notes);
 });
 
+// Route to add new notes
+app.post("/notes", (req, res) => {
+  const note = {
+    id: Date.now(),
+    text: req.body.text
+  };
+
+  notes.push(note);
+  res.json(note);
+});
+
 app.listen(5000, () => 
 {
     console.log("Server running on port : 5000");
